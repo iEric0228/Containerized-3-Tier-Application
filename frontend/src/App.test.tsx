a7263c506d2b24c19665eecc287b7606aa8d1c8f
iEric0228 <118494002+iEric0228@users.noreply.github.com>
2026-01-04 21:03:50 -0500

    Fixed eslint and package.json issues

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders DevOps Portfolio dashboard', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Initializing DevOps Portfolio/i);
+  expect(headingElement).toBeInTheDocument();
 });
